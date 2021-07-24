072407360a001362498a028713e26bc2fea7d3d8
Mohammad Abdulwahhab <Mohammadhawani8@gmail.com>
2021-07-24 15:56:19 -0400

    add button in the add item window

--- src/main/java/ucf/assignments/AddItemController.java
+++ src/main/java/ucf/assignments/AddItemController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class AddItemController {
 
@@ -28,6 +29,17 @@ public class AddItemController {
 
     @FXML
     void addButtonClicked(ActionEvent event) {
+
+        // add the new item with the given parameters
+        // clear the text boxes 
+        // get the current stage and close it
+
+        this.model.addItem(itemNameText.getText(), serialNumberText.getText(), Double.parseDouble(itemValueText.getText()));
+        Stage currentStage = (Stage)addButton.getScene().getWindow();
+        serialNumberText.clear();
+        itemValueText.clear();
+        itemNameText.clear();
+        currentStage.close();
 
     }
 
